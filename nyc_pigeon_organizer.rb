d477797f42cab8c2c2c600814309b0e09f4650aa
Sam Evans <evansst@icloud.com>
2020-06-16 04:56:02 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -4,7 +4,7 @@ def nyc_pigeon_organizer(data)
   data.each_with_object({}) do |(key, value), final_array|
     value.each do |inner_key, names|
       names.each do |name|
-        final_array[name][key]
+        final_array[name][key].push(inner_key.to_s)
     
   
 end
