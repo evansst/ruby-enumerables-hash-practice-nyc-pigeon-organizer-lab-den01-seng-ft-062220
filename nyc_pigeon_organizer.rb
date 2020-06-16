a68749eb5230667dff5e4c2514b762cc16befba0
Sam Evans <evansst@icloud.com>
2020-06-16 05:06:08 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -11,7 +11,7 @@ def nyc_pigeon_organizer(data)
         if !final_array[name][key]
           final_array[name][key] = []
         end
-        binding.pry
+        # binding.pry
         final_array[name][key].push(inner_key.to_s)
         # binding.pry
       end
